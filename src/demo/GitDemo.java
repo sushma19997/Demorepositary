4adf092ae1171721f24302e05629d300cd9bcfb4
Lenovo <Lenovo@DESKTOP-ORVKQNT>
2022-11-14 16:35:24 +0530

    DEMO22

---
+++ src/demo/GitDemo.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class GitDemo {
+
+	
+	public static void main(String[] args) {
+		String name="sushma";
+		int rollno=101;
+		System.out.println(name);
+		System.out.println(rollno);
+		
+		
+			
+		}
+		
+	}
+
+
